c0de61f9d2f99db224022068d82083d334d0b48a
ezexgonzalez <ezexgonzalez@outlook.com>
2022-03-09 19:11:06 -0300

    responsive mobile

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -9,26 +9,34 @@ export default function Main() {
     return (
         <main id="main" className={s.main}>
             <div className={s.presentation}>
-               <div className={s.redes}>
+                <div className={s.redes}>
                     <a className={s.link} rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/ezexgonzalez/">
                         <UilLinkedinAlt className={s.redIcon} />
                     </a>
                     <a className={s.link} rel="noreferrer" target="_blank" href="https://github.com/ezexgonzalez/">
                         <UilGithubAlt />
                     </a>
-                </div> 
+                </div>
                 <div className={s.info}>
                     <div className={s.titleContainer}>
                         <span className={s.title}>Ezequiel</span>
                         <span className={s.title}>Gonzalez</span>
                         <span className={s.subTitle}>Full Stack Developer</span>
                     </div>
+                    <div className={s.redesMobile}>
+                            <a className={s.linkM} rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/ezexgonzalez/">
+                                <UilLinkedinAlt className={s.redIcon} />
+                            </a>
+                            <a className={s.linkM} rel="noreferrer" target="_blank" href="https://github.com/ezexgonzalez/">
+                                <UilGithubAlt />
+                            </a>
+                        </div>
                     <div className={s.pContainer}>
                         <p className={s.resume}>Con formación y experiencia en el desarrollo de paginas web como programador Full Stack</p>
-                        <button className={s.mainButton}>
+                        <Link activeClass="active" to="contact" spy={true} smooth={true} offset={50} duration={800} className={s.mainButton}>
                             Conctáctame
                             <UilNavigator className={s.buttonIcon} />
-                        </button>
+                        </Link>
                     </div>
                     <div className={s.scrollContainer}>
                         <Link className={s.effect} activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={800}>
@@ -44,7 +52,7 @@ export default function Main() {
             </div>
             */
             }
-            <ScrollToTop/>
+           {/*  <ScrollToTop /> */}
         </main>
     )
 
